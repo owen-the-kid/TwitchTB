eb4f53117fc90eac997c246632c56866802ed34b
owen-the-kid <owenramaekers2006@gmail.com>
2025-04-25 23:41:55 +0200

    first test

---
+++ scripts/userScript.js
@@ -0,0 +1,4 @@
+const script = document.createElement('script');
+script.src = 'https://raw.githubusercontent.com/pixeltris/TwitchAdSolutions/0b5ea5ed8959a6b4eb4c1ea406aaa56313c9c907/vaft/vaft-ublock-origin.js';
+document.head.appendChild(script);
+
