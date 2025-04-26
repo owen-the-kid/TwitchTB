753c6db91d42f5e4f892dcbe3543df0598c3b8d5
OwenTheKid <84964772+owen-the-kid@users.noreply.github.com>
2025-04-26 17:51:29 +0200

    Remove broke thing

--- scripts/userScript.js
+++ scripts/userScript.js
@@ -2,19 +2,6 @@ import { FiltersEngine } from '@ghostery/adblocker';
 const engine = FiltersEngine.parse(fs.readFileSync('easylist.txt', 'utf-8'));
 //Ghostery adblocker
 
-document.addEventListener('keydown', function(event) {
-    if (event.key === 'Back') {
-        // Emulate the ESC key
-        var escEvent = new KeyboardEvent('keydown', {
-            key: 'Escape',
-            code: 'Escape',
-            keyCode: 27, // Deprecated, but some old browsers might still require it
-            which: 27, // Deprecated, but some old browsers might still require it
-            bubbles: true,
-            cancelable: true
-        });
-        document.dispatchEvent(escEvent);
-    }
-});
+
 
 
