d34473f04fde79bcca00dd39da5765aabf440bd6
owen-the-kid <owenramaekers2006@gmail.com>
2025-04-27 14:14:05 +0200

    mention

--- scripts/userScript.js
+++ scripts/userScript.js
@@ -2,10 +2,11 @@ import { FiltersEngine } from '@ghostery/adblocker';
 engine = await FiltersEngine.fromLists(fetch, [
  'https://easylist.to/easylist/easylist.txt'
 ]);
-
+//code above blocks ads and is powered by ghostery
 // ESC Key emualtion for back button
 document.addEventListener('back', (event) => {
   if (event.key === 'Escape') {
     window.history.back();
   }
 });
+
