132768431276d1aab00840101937b06ee85ce1e7
owen-the-kid <owenramaekers2006@gmail.com>
2025-04-27 14:06:18 +0200

    maybe works

--- scripts/userScript.js
+++ scripts/userScript.js
@@ -2,3 +2,10 @@ import { FiltersEngine } from '@ghostery/adblocker';
 engine = await FiltersEngine.fromLists(fetch, [
  'https://easylist.to/easylist/easylist.txt'
 ]);
+
+// ESC Key emualtion for back button
+document.addEventListener('back', (event) => {
+  if (event.key === 'Escape') {
+    window.history.back();
+  }
+});
