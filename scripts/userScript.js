371212eb1b174229fd61874877be138e2e49d1bc
owen-the-kid <owenramaekers2006@gmail.com>
2025-04-30 15:14:26 +0200

    fix

--- scripts/userScript.js
+++ scripts/userScript.js
@@ -1,9 +1,7 @@
 // Install uBlock Origin core via npm
-// npm install --save-dev ubo-core
 
-const { filterListFromText } = require('ubo-core');
+const { StaticNetFilteringEngine } = require('@gorhill/ubo-core');
 
-// Example of using a blocklist
 const blocklistText = `
 ||video-edge-*.ttvnw.net/v1/segment/*$media
 ||video-edge-d55370.ams02.abs.hls.ttvnw.net/v1/segment/*$media
@@ -12,7 +10,8 @@ const blocklistText = `
 ||imasdk.googleapis.com/js/sdkloader/ima3.js$domain=twitch.tv,important
 `;
 
-const filterList = filterListFromText(blocklistText);
+const snfe = await StaticNetFilteringEngine.create();
+await snfe.useLists([ { raw: blocklistText } ]);
 
 // Apply the filter list as needed in your application
 
